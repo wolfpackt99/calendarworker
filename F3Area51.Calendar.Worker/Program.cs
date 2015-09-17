569bcf799da1110be360b64dd62df114af1fffbe
Trent <trent.jones>
2015-09-17 17:07:48 -0400

    Initial Commit

---
+++ F3Area51.Calendar.Worker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using F3.Business.Calendar;
+using Google.Apis.Calendar.v3.Data;
+
+namespace F3Area51.Calendar.Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                DoSomething();
+                Thread.Sleep(60 * 1000);
+            }
+        }
+
+        private static void DoSomething()
+        {
+            var x = new CalendarBusiness();
+            var result = x.Publish();
+        }
+    }
+}
